d4a75f10f9e30bed38d62a14854d5eb62c11c7e3
Piquin-Dylan <dylan.piquin@student.hepl.be>
2026-06-30 22:00:40 +0200

    ajout de la middleware auth

--- routes/web.php
+++ routes/web.php
@@ -5,23 +5,24 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/admin', function () {
     return view('components.pages.login');
+})->name('login');
+
+
+Route::middleware('auth')->group(function () {
+
+    Route::get('/dashboard', function () {
+        return view('components.pages.dashboard');
+    });
+
+    Route::get('/animals', function () {
+        return view('components.pages.animals.animalsList');
+    })->name('animals');
+
+    Route::get('/animals/create', function () {
+        return view('components.pages.animals.animals');
+    })->name('animals.create');
+
+    Route::get('/animals/{id}', function () {
+        return view('components.pages.animals.animalsShow');
+    })->name('animals.show');
 });
-
-
-
-
-Route::get('/dashboard', function () {
-    return view('components.pages.dashboard');
-});
-
-Route::get('/animals', function () {
-    return view('components.pages.animals.animalsList');
-})->name('animals');
-
-Route::get('/animals/create', function () {
-    return view('components.pages.animals.animals');
-})->name('animals.create');
-
-Route::get('/animals/{id}', function () {
-    return view('components.pages.animals.animalsShow');
-})->name('animals.show');
